fc77c47d193551b6703c8d674f967e90a26ae6d1
Zachary Lehmann <zlehmann@gmail.com>
2018-10-04 00:52:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,11 +18,13 @@ class Transfer
   end
 
   def execute_transaction
+    binding.pry
     if self.valid? == true && @sender.balance >= @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
