dc558fe0643de0359e7c5dd975ed43b07c2c9047
Zachary Lehmann <zlehmann@gmail.com>
2018-10-04 00:41:14 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,18 +1,18 @@
 class Transfer
   # your code here
-  attr_accessor :sender, :reciever, :amount, :status
+  attr_accessor :sender, :receiver, :amount, :status
 
-  def initialize(sender, reciever, amount)
+  def initialize(sender, receiver, amount)
     @sender = sender
-    @reciever = reciever
+    @receiver = receiver
     @amount = 50
     @status = "pending"
   end
 
   def valid?(sender, reciever, amount)
     sender_is_valid = sender.valid?
-    reciever_is_valid = reciever.valid?
-    if sender_is_valid == true && reciever_is_valid == true
+    receiver_is_valid = receiver.valid?
+    if sender_is_valid == true && receiver_is_valid == true
       return true
     else
       return false
